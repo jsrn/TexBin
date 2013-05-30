bda31dead2be93cb1296c8ba129bdbbce688fef0
James Nutt <james.srn@gmail.com>
2013-05-30 13:20:24 +0100

    Delete surplus files

--- app/texbin.php
+++ app/texbin.php
@@ -5,11 +5,24 @@ class TeXBin
 	function processTeX()
 	{
 		$rawTex = $_POST['tex'];
-		$newTexFile = time() . '.tex'; 
+
+		$filename = time();
+
+		$newTexFile = "$filename.tex"; 
+
 		$texWriter = fopen($newTexFile, 'w') or die("can't open file");
 
 		fwrite($texWriter, $rawTex);
+
 	    fflush($texWriter);
+
+	    fclose($texWriter);
+
+	    exec("C:/Progra~2/miktex~1.9/miktex/bin/pdflatex.exe " . $newTexFile);
+
+	    $pdfFile = "$filename.pdf";
+
+	    TeXBin::cleanUp( $filename );
 	}
 
 	function getPDF( $filename )
@@ -21,6 +34,13 @@ class TeXBin
 	function deletePDF( $filename )
 	{
 		unlink("$filename.pdf");
+	}
+
+	function cleanUp( $filename )
+	{
+		unlink( "$filename.log" );
+		unlink( "$filename.aux" );
+		unlink( "$filename.tex" );
 	}
 }
 
