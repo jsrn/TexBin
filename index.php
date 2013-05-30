5272acfda5353d61904bc34b5548ae92f7d6ab81
James Nutt <james.srn@gmail.com>
2013-05-30 11:18:43 +0100

    Added index and main, with base template

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>TexBin | TeX goes in, PDF comes out</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+
+	<div id="content">
+		<div id="header">
+			TexBin - A quick and dirty web front end for pdflatex.
+		</div>
+
+		<textarea id="tex-field"></textarea>
+	</div>
+
+	<!-- GitHub fork button -->
+	<a target="_blank" href="https://github.com/jsrn/TexBin"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png" alt="Fork me on GitHub"></a>
+	
+</body>
+</html>
