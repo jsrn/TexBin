cd0ee0ac58bd2f5d046e5d86c6e1e09c78a81ecd
James Nutt <james.srn@gmail.com>
2013-05-30 12:04:34 +0100

    Changed banner

--- public/index.php
+++ public/index.php
@@ -28,7 +28,7 @@ Hello, world!
 	</div>
 
 	<!-- GitHub fork button -->
-	<a target="_blank" href="https://github.com/jsrn/TexBin"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png" alt="Fork me on GitHub"></a>
+	<a target="_blank" href="https://github.com/jsrn/TexBin"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png" alt="Fork me on GitHub"></a>
 	
 </body>
 </html>
