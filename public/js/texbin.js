9cea833360a0ccd669bdd41792c545866f8bfa55
James Nutt <james.srn@gmail.com>
2013-05-30 14:06:10 +0100

    Add mode value

--- public/js/texbin.js
+++ public/js/texbin.js
@@ -2,5 +2,8 @@ function sendTeX()
 {
 	var tex = $("#tex-field").val();
 	
-	$('<form action="texbin.php" style="display: none;" method="POST" target="_blank"><textarea name="tex">' + tex + '</textarea></form>').appendTo('body').submit();
+	$('<form action="texbin.php" style="display: none;" method="POST" target="_blank">'
+		+'<textarea name="tex">' + tex + '</textarea>'
+		+'<input name="mode" value="internal">'
+		+'</form>').appendTo('body').submit();
 }
