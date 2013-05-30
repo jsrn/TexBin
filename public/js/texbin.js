fe4d166aa9f7a606c63a484c7d08083ed26ddf48
James Nutt <james.srn@gmail.com>
2013-05-30 14:12:05 +0100

    Dumped mode

--- public/js/texbin.js
+++ public/js/texbin.js
@@ -4,6 +4,5 @@ function sendTeX()
 	
 	$('<form action="texbin.php" style="display: none;" method="POST" target="_blank">'
 		+'<textarea name="tex">' + tex + '</textarea>'
-		+'<input name="mode" value="internal">'
 		+'</form>').appendTo('body').submit();
 }
