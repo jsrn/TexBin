a34b030c15854d64fc80ecd5eaec93bfc790adfc
James Nutt <james.srn@gmail.com>
2013-05-30 12:40:59 +0100

    JS and onclick

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@
 	<meta charset="UTF-8">
 	<title>TexBin | TeX goes in, PDF comes out</title>
 	<link rel="stylesheet" type="text/css" href="main.css">
+	<script src="jquery-1.10.0.min.js"></script>
+	<script src="texbin.js"></script>
 </head>
 
 <body>
@@ -23,12 +25,12 @@ Hello, world!
 \end{document}</textarea>
 
 		<div id="control-bar">
-			<button type="button">PDF ME!</button>
+			<button type="button" onclick="sendTeX()">PDF ME!</button>
 		</div>
 	</div>
 
 	<!-- GitHub fork button -->
 	<a target="_blank" href="https://github.com/jsrn/TexBin"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png" alt="Fork me on GitHub"></a>
-	
+
 </body>
 </html>
