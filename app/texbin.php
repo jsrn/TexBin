a3389e564ef5e24a59aae8b45c8571b233318174
James Nutt <james.srn@gmail.com>
2013-05-30 12:44:08 +0100

    Moved texbin functions into class

--- app/texbin.php
+++ app/texbin.php
@@ -1,24 +1,27 @@
 <?php
 
-function processTeX()
+class TeXBin
 {
-	$rawTex = $_POST['tex'];
-	$newTexFile = time() . '.tex'; 
-	$texWriter = fopen($newTexFile, 'w') or die("can't open file");
+	function processTeX()
+	{
+		$rawTex = $_POST['tex'];
+		$newTexFile = time() . '.tex'; 
+		$texWriter = fopen($newTexFile, 'w') or die("can't open file");
 
-	fwrite($texWriter, $rawTex);
-    fflush($texWriter);
-}
+		fwrite($texWriter, $rawTex);
+	    fflush($texWriter);
+	}
 
-function getPDF( $filename )
-{
-	header( "Content-type: application/pdf" );
-    echo( file_get_contents( "$filename.pdf" ) );
-}
+	function getPDF( $filename )
+	{
+		header( "Content-type: application/pdf" );
+	    echo( file_get_contents( "$filename.pdf" ) );
+	}
 
-function deletePDF( $filename )
-{
-	unlink("$filename.pdf");
+	function deletePDF( $filename )
+	{
+		unlink("$filename.pdf");
+	}
 }
 
 ?>
